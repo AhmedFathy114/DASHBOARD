ab083a2e9ddd4c2a3f54cd96202e13b7047745e4
AhmedFathy114 <af3611038@gmail.com>
2025-12-08 00:59:07 +0200

    delete unused files

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-import { useRoutes } from 'react-router-dom'
-import './App.css'
-import { routes } from './Routes'
-function App() {
-    const elements = useRoutes(routes);
-    return elements;
-}
-
-export default App
--- src/utils/RouterHelper.jsx
+++
@@ -1,6 +0,0 @@
-import DashboardLayout from "../layouts/Dashboard"
-
-function WithDashLayout(page){
-    return <DashboardLayout>{page}</DashboardLayout>
-}
-export default WithDashLayout
